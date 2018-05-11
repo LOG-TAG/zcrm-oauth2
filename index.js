023af7e94645f9a8d467a7034d06e3c813988993
Dom <domenico.ruggiano@outlook.it>
2018-05-11 17:57:54 +0200

    aggiungo la possibilità di settare il server Zoho

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ const template = require('./config/template');
 program
   .version(version)
   .option('-f, --file <file>', 'auth_file is required', main)
+  .option('-s, --server <server>', 'eu || com, default: eu', supportedServers)
   .parse(process.argv);
 
 if (!program.file) error('Devi fornire in input un file');
+console.log(`Server Zoho: ${program.server}`);
+
+function supportedServers(server) {
+  switch (server) {
+    case 'eu':
+    case 'com':
+      return server;
+    default:
+      console.log(`Server '${server}' invalido, utilizzo: 'eu'`);
+      return 'eu';
+  }
+}
 
 function main(file) {
   const validation = validateFile(file);
@@ -53,7 +66,7 @@ function error(error) {
 
 function sendRequest(options) {
   const { grant, redirect_uri, client_id, client_secret } = options;
-  request.post(`https://accounts.zoho.com/oauth/v2/token?code=${grant}&redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code`,
+  request.post(`https://accounts.zoho.${program.server}/oauth/v2/token?code=${grant}&redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code`,
     (err, resp, body) => {
       if (err) error(`Errore nell'ottenere una risposta da Zoho: ${err.message}`);
       fs.writeFileSync('out.json', body);
