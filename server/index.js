cbb00f9da12aa56f7b398524238fc7c7f8895117
Dom <domenico.ruggiano@outlook.it>
2018-05-12 10:38:19 +0200

    server port as function parameter

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const app = require('express')();
-
-const port = process.env.PORT || 8000;
 let server;
 
-function makeServer(onCodeReceived) {
+function makeServer(port, onCodeReceived) {
   app.get('/callback', (req, res) => {
     console.log(req);
     onCodeReceived(req.query.code);
     res.send('You can close the browser now.');
 
     if (server) server.close(() => {
-      console.log('server closed');
+      console.log('Server closed');
     });
   });
 
