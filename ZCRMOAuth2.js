0b0384517be835ff830ded4d578c48804891aa9c
Dom <domenico.ruggiano@outlook.it>
2018-05-17 09:06:34 +0200

    Code cleanup

--- ZCRMOAuth2.js
+++ ZCRMOAuth2.js
@@ -60,35 +60,39 @@ else
     getTokens
   );
 
+/**
+ * Verify that all the required options have been set.
+ * If the options are imported from file, joins the JSON obj with 'program'.
+ * @param {Object} program
+ * @returns {Object} - list of options
+ */
+
 function validateOptions(program) {
   const importFromFile = program.file || false;
   let validation = importFromFile ? validateFile(importFromFile) : program;
 
+  // check if any of the required options is undefiend
   const requiredOptions = ['id', 'secret', 'redirect'];
-
-  // check if any of the required fields is undefiend
   const missing = requiredOptions.filter(item => typeof validation[item] === 'undefined');
-
   if (missing.length)
     error(
       `You must specify valid ${missing.join(', ')}.`,
       `Run ${chalk.cyan(`${packageJSON.name} --help`)} to see all options.`
     );
 
-  // check if user wants to generate code but is using another redirect then "localhost"
+  // check if user wants to generate the grant code but is using another redirect rather then "localhost"
   if (!validation.code && validation.redirect && url.parse(validation.redirect).hostname !== 'localhost')
     error(`You must use a "localhost" redirect if you want to generate the code "grant_token".`);
 
   return Object.assign({}, program, validation);
 }
 
-function generateOutputFileName() {
-  const now = new Date();
-  const twoDigits = data => `0${data}`.slice(-2);
-  const date = `${now.getFullYear()}-${twoDigits(now.getMonth() + 1)}-${twoDigits(now.getDate())}`;
-  const time = `${twoDigits(now.getHours())}-${twoDigits(now.getMinutes())}-${twoDigits(now.getSeconds())}`;
-  return `out-${date}T${time}.json`;
-}
+/**
+ * Verify that the file passed as option is an existing file, is readable and
+ * can be parse as JSON file.
+ * @param {string} file - file path given with --file option
+ * @returns {Object} - object parsed from JSON
+ */
 
 function validateFile(file) {
   try {
@@ -117,6 +121,25 @@ function validateFile(file) {
   }
 }
 
+/**
+ * Generates an output file named based on the current datetime.
+ * @returns {string} - generated output file name
+ */
+
+function generateOutputFileName() {
+  const now = new Date();
+  const twoDigits = data => `0${data}`.slice(-2);
+  const date = `${now.getFullYear()}-${twoDigits(now.getMonth() + 1)}-${twoDigits(now.getDate())}`;
+  const time = `${twoDigits(now.getHours())}-${twoDigits(now.getMinutes())}-${twoDigits(now.getSeconds())}`;
+  return `out-${date}T${time}.json`;
+}
+
+/**
+ * Helper function that exits the process with code 1.
+ * @param {string} error - error message
+ * @param {string=} suggestion - optional suggestion string
+ */
+
 function error(error, suggestion) {
   console.log(chalk.bgRed(error));
 
@@ -127,6 +150,12 @@ function error(error, suggestion) {
 
   process.exit(1);
 }
+
+/**
+ * Make a POST request to OAuth2 Zoho CRM endpoint to get
+ * access and refresh tokens.
+ * @param {string} code - grant_token required to generate access and refresh tokens
+ */
 
 function getTokens(code) {
   const qs = {
@@ -148,6 +177,11 @@ function getTokens(code) {
       writeOutputFile(body);
     });
 }
+
+/**
+ * Write the content into output file.
+ * @param {string} content
+ */
 
 function writeOutputFile(content) {
   content = JSON.stringify(JSON.parse(content), null, 4); // formatting JSON
